88928bf5bf3d508245ee312070e0ea08f7445897
Ignacy K <132497046+ignxcy@users.noreply.github.com>
2023-07-29 11:41:49 +0000

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,15 @@
-const commandForm = document.getElementById('commandForm');
-const cmdInput = document.getElementById('cmd');
-const outputDiv = document.getElementById('output');
-
-commandForm.addEventListener('submit', async function(event) {
+document.getElementById("commandForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    const cmd = cmdInput.value.trim();
-    if (cmd === '') {
-        outputDiv.innerHTML = 'Please enter a command.';
-        return;
-    }
 
-    try {
-        const response = await fetch(`http://linrun.sparkklol.repl.co/execute?cmd=${encodeURIComponent(cmd)}`);
-        const data = await response.json();
-        if (response.ok) {
-            outputDiv.innerHTML = data.result;
-        } else {
-            outputDiv.innerHTML = data.error || 'An unknown error occurred.';
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        outputDiv.innerHTML = 'An error occurred while processing the request.';
-    }
-});
+    const command = document.getElementById("command").value;
+    const url = `https://linrun.sparkklol.repl.co/execute?cmd=${command}`;
+
+    fetch(url)
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  });
